1f2b75c794be566285ae51d582cd9275ca352cee
Timothée Jourde <itim.lcf@gmail.com>
2018-09-07 20:33:26 +0200

    doc: internship report

---
+++ doc/default.nix
@@ -0,0 +1,45 @@
+let
+  rev    = "ce0d9d638ded6119f19d87e433e160603683fb1b"; # nixos-18.03
+  sha256 = "0na6kjk4xw6gqrn3a903yv3zfa64bspq2q3kd6wyf52y44j3s8sx";
+
+  repo = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/${rev}.tar.gz";
+    inherit sha256;
+  };
+  nixpkgs = import repo {};
+
+in with nixpkgs; stdenv.mkDerivation rec {
+  file = "mastermind";
+  
+  name = "${file}.pdf";
+  
+  src = ./.;
+  
+  buildInputs = [
+    (texlive.combine { inherit (texlive)
+      scheme-basic
+      babel-french
+      tabu varwidth
+      xcolor
+      parskip enumitem sectsty
+      footmisc
+      tocloft
+      mathtools
+      cancel
+      libertine xkeyval fontaxes
+      inconsolata upquote
+      euler
+      tikz-qtree pgf
+      # qtree pict2e
+    ;})
+  ];
+
+  #HOME = ".";
+  
+  buildPhase = ''
+    pdflatex -draftmode ${file}.tex
+    pdflatex ${file}.tex
+  '';
+  
+  installPhase = "cp ${file}.pdf $out";
+}
